abfb627900ab434b136bf622a6dd126069bf702c
s10singh97 <s10singh97@yahoo.in>
2018-10-05 15:19:41 +0530

    login function completed

--- Project/app.py
+++ Project/app.py
@@ -130,3 +130,39 @@ def history():
     histories = cursor.execute("SELECT * from histories WHERE id=:id", id=session["user_id"])
 
     return render_template("history.html", histories=histories)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Must provide username")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Must provide password")
+
+        # query database for username
+        rows = cursor.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # ensure username exists and password is correct
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+            return apology("invalid username and/or password")
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
