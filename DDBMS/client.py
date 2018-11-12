bcaced81d21bb2f0b0de87098749c2f27618117f
s10singh97 <s10singh97@yahoo.in>
2018-11-13 01:02:05 +0530

    client side

--- DDBMS/client.py
+++ DDBMS/client.py
@@ -1,8 +1,15 @@
-import socket
-
-s = socket.socket()
-host = socket.gethostname()
-print(host)
-port = 9077
-s.connect((host, port))
-print(s.recv(1024).decode("utf-8"))
+import os
+from socket import *
+host = "172.21.4.55" # set to IP address of target computer
+port = 13000
+addr = (host, port)
+addr1 = ("", 13001)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    data = input("Enter message to send or type 'exit': ")
+    UDPSock.sendto(data.encode("utf-8"), addr)
+    result, addr1 = UDPSock.recvfrom(1024)
+    print(result)
+    if data == "exit":
+        break
+UDPSock.close()
