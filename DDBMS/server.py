1d3341aa04e83ad5baf7522201e6de8827cbae04
s10singh97 <s10singh97@yahoo.in>
2018-11-13 01:02:45 +0530

    server side

--- DDBMS/server.py
+++ DDBMS/server.py
@@ -1,15 +1,26 @@
-import socket
 import os
+import sqlite3
+from socket import *
 
-s = socket.socket()
-host = socket.gethostname()
-port = 9077
-s.bind((host,port))
-s.listen(5)
+conn = sqlite3.connect("testdb.db")
 
+host = gethostname()
+port = 13001
+buf = 1024
+addr = (host, port)
+print(host)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+UDPSock.bind(addr)
+print("Waiting to receive messages...")
 while True:
-    c, addr = s.accept()
-    print("Connection accepted from " + repr(addr[1]))
-    c.send(b'Thank you for connecting')
-    # c.send(b'mkdir HI')
-    c.close()
+    data, addr = UDPSock.recvfrom(buf)
+    print("Received message: " + data.decode("utf-8"))
+    if data == "exit":
+        break
+    query = data.decode("utf-8")
+    result = conn.execute(query)
+    conn.execute("commit;")
+    for row in result:
+        print(row)
+    # UDPSock.sendall(result.encode("utf-8"))
+UDPSock.close()
