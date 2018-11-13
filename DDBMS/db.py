1ba5aa637e55d05e085adb8a1b6a522fc76b96c3
s10singh97 <s10singh97@yahoo.in>
2018-11-14 00:46:35 +0530

    adding common database

--- DDBMS/db.py
+++ DDBMS/db.py
@@ -1,10 +1,18 @@
 import sqlite3 as sql
 
 conn = sql.connect("testdb.db")
+conn2 = sql.connect("common.db")
 if conn:
-    print("Successful conncetion!!")
+    print("Successful conncetion 1!!")
 else:
-    print("Unsuccessful Connection")
+    print("Unsuccessful Connection 1")
 conn.execute("CREATE TABLE IF NOT EXISTS test(id int)")
 conn.execute("INSERT INTO test VALUES(55)")
 conn.commit()
+
+conn2.execute("CREATE TABLE IF NOT EXISTS co(id int, name varchar(25))")
+conn2.execute("INSERT INTO co VALUES(1, 'abc')")
+conn2.commit()
+
+conn.close()
+conn2.close()
